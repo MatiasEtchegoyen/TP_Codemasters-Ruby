a85dd1b1780bf2ed28c89de8fd27e47bdc0e091d
tomasghiglione <tghiglione@fi.uba.ar>
2024-12-01 10:45:49 -0300

    logueo segun usuario

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
     resources :favorites, only: [:index, :create, :destroy]
 
     resources :products
+    get '/login_selection', to: 'sessions#selection', as: 'login_selection'
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
