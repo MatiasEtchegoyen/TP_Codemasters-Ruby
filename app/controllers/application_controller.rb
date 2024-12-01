e34c14b14f8a32460ed2f540cb51686daab688b6
tomasghiglione <tghiglione@fi.uba.ar>
2024-12-01 10:54:45 -0300

    redireccion admin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :set_current_cart
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_path 
+    elsif resource.is_a?(Purchaser)
+      products_path  
+    else
+      super  
+    end
+  end
   protected
   def check_admin
     if !current_admin
